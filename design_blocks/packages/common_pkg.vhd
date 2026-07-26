9be6debc95be11a2a98e12a748cada3b12e46030
Kevin Toledo Fernandez <ktoledo0456@gmail.com>
2026-07-25 19:56:47 -0600

    Fixed issues with PI controller

--- design_blocks/packages/common_pkg.vhd
+++ design_blocks/packages/common_pkg.vhd
@@ -3,25 +3,33 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 package common_pkg is 
+    -- System constants
     constant C_CLK_FREQ             : real      := 50.0e6;      -- 50 Mhz clock 
     constant C_PWM_FREQ             : real      := 20.0e3;      -- 20 kHz PWM frequency
     constant C_CLKS_IN_PWM_PRD      : natural   := integer(C_CLK_FREQ / C_PWM_FREQ);
+    constant C_VBUS                 : real      := 24.0;        -- 24V bus voltage
+    constant C_MAX_VOLT             : real      := C_VBUS / sqrt(3.0);
 
     type t_phase_current is sfixed(3 downto -12);   -- Max value: +7.9997 Amps, Min value: -8.0000 Amps
     type t_phase_voltage is sfixed(5 downto -12);   -- Max value: +31.9997 Volts, Min value: -32.0000 Volts
 
     type t_ph_volt_record is record 
         vld         : std_logic;
-        A           : t_ph_volt_record;
-        B           : t_ph_volt_record;
-        C           : t_ph_volt_record;
+        A           : t_phase_voltage;
+        B           : t_phase_voltage;
+        C           : t_phase_voltage;
     end record t_ph_volt_record; 
 
     type t_clarke_volt_record is record 
         vld         : std_logic;
-        alpha       : t_ph_volt_record;
-        beta        : t_ph_volt_record;
+        alpha       : t_phase_voltage;
+        beta        : t_phase_voltage;
     end record t_clarke_volt_record;
+
+    type t_park_volt_record is record 
+        vld         : std_logic;
+        d           : t_phase_voltage;
+        q           : t_phase_voltage;
 
     -- Interface records
     type t_phase_record is record 
--- design_blocks/pi_control/pi_control.vhd
+++ design_blocks/pi_control/pi_control.vhd
@@ -1,3 +1,10 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.fixed_pkg.all;
+use work.common_pkg.all;
+use work.cloop_pkg.all;
+
 entity pi_control is
     port (
         -- System
@@ -5,34 +12,43 @@ entity pi_control is
         i_rst           : in std_logic;
         
         -- Input from Park transformation 
-        i_rdy           : in std_logic;
-        i_target_curr   : in t_phase_record;
+        i_vld           : in std_logic;
+        i_target_curr   : in t_park_record;
 
-        -- Inputs from target and actual current
-        i_actual_curr   : in t_phase_record;
+        -- Inputs from encoder reading of current
+        i_actual_curr   : in t_park_record;
 
         -- Output to SVPWM
-        o_ph_voltage    : out t_clarke_volt_record
+        o_vld           : out std_logic;
+        o_ph_voltage    : out t_park_volt_record;
+
+        -- Ready for new sample 
+        o_rdy           : out std_logic
     );
 end entity pi_control;
 
 architecture rtl of pi_control is
     type t_pi_controller_states is (
         ST_IDLE,
-        ST_MATH,
+        ST_ERROR,
         ST_GAIN,
-        ST_WINDUP
+        ST_ACCUM,
+        ST_OUTPUT
     );
     
-    signal r_state      : t_pi_controller_states;
-    signal w_nx_state   : t_pi_controller_states;
+    signal r_state          : t_pi_controller_states;
+    signal w_nx_state       : t_pi_controller_states;
 
-    signal r_target_curr    : t_phase_record;
-    signal r_actual_curr    : t_phase_record;
-    signal r_error_curr     : t_phase_record;
-    signal r_gain_prop      : t_phase_record;
-    signal r_gain_intg      : t_phase_record;
+    signal r_target_curr    : t_park_record;
+    signal r_actual_curr    : t_park_record;
+    signal r_error_curr     : t_park_record;
+    signal r_gain_prop      : t_park_volt_record;
+    signal r_gain_intg      : t_park_volt_record;
+    signal r_intg_accum     : t_park_volt_record;
+    signal r_volt_next      : t_park_volt_record;
 begin
+    o_rdy <= '1' when r_state = ST_IDLE else '0';
+
     fsm : process(i_clk)
     begin 
         if rising_edge(i_clk) then 
@@ -50,18 +66,21 @@ begin
 
         case r_state is
             when ST_IDLE => 
-                if i_rdy = '1' then 
-                    w_nx_state <= ST_MATH;
+                if i_vld = '1' then 
+                    w_nx_state <= ST_ERROR;
                 end if;
 
             when ST_ERROR => 
                 w_nx_state <= ST_GAIN;
 
             when ST_GAIN => 
-                w_nx_state <= ST_WINDUP;
+                w_nx_state <= ST_ACCUM;
 
-            when ST_WINDUP => 
-                when w_nx_state <= ST_IDLE;
+            when ST_ACCUM => 
+                w_nx_state <= ST_OUTPUT;
+
+            when ST_OUTPUT => 
+                w_nx_state <= ST_IDLE;
 
             when others =>
                 w_nx_state <= ST_IDLE;
@@ -74,7 +93,7 @@ begin
             if i_rst = '1' then 
                 r_target_curr       <= (others => (others => '0'));
                 r_actual_curr       <= (others => (others => '0'));
-            elsif r_state = ST_IDLE and i_rdy = '1' then 
+            elsif r_state = ST_IDLE and i_vld = '1' then 
                 r_target_curr       <= i_target_curr;
                 r_actual_curr       <= i_actual_curr;
             end if;
@@ -85,12 +104,10 @@ begin
     begin 
         if rising_edge(i_clk) then 
             if i_rst = '1' then 
-                r_error_curr.vld    <= (0 => '0', others => (others => '0'));
-            elsif r_state = ST_IDLE and i_rdy = '1' then 
-                r_error_curr.vld    <= '1';
-                r_error_curr.A      <= resize(r_target_curr.A - r_actual_curr.A, r_error_curr.A);
-                r_error_curr.B      <= resize(r_target_curr.B - r_actual_curr.B, r_error_curr.B);
-                r_error_curr.C      <= resize(r_target_curr.C - r_actual_curr.C, r_error_curr.C);
+                r_error_curr        <= (others => (others => '0'));
+            elsif r_state = ST_ERROR then 
+                r_error_curr.d      <= resize(r_target_curr.d - r_actual_curr.d, r_error_curr.d);
+                r_error_curr.q      <= resize(r_target_curr.q - r_actual_curr.q, r_error_curr.q);
             end if;
         end if;
     end process error;
@@ -99,22 +116,81 @@ begin
     begin 
         if rising_edge(i_clk) then 
             if i_rst = '1' then 
-                r_gain_mult         <= (0 => '0', others => (others => '0'));
-                r_gain_intg         <= (0 => '0', others => (others => '0'));
-            else 
-                r_gain_mult.vld     <= '1';
-                r_gain_mult.A       <= resize(r_error_curr.A * C_KP_PROP_GAIN, r_gain_mult.A);
-                r_gain_mult.B       <= resize(r_error_curr.B * C_KP_PROP_GAIN, r_gain_mult.B);
-                r_gain_mult.C       <= resize(r_error_curr.C * C_KP_PROP_GAIN, r_gain_mult.C);
+                r_gain_prop         <= (others => (others => '0'));
+                r_gain_intg         <= (others => (others => '0'));
+            elsif r_state = ST_GAIN then 
+                r_gain_prop.d       <= resize(r_error_curr.d * C_KP_PROP_GAIN, r_gain_prop.d);
+                r_gain_prop.q       <= resize(r_error_curr.q * C_KP_PROP_GAIN, r_gain_prop.q);
 
-                r_gain_intg.vld     <= '1';
-                r_gain_intg.A       <= resize(r_error_curr.A * C_KI_INTG_GAIN, r_gain_intg.A);
-                r_gain_intg.B       <= resize(r_error_curr.B * C_KI_INTG_GAIN, r_gain_intg.B);
-                r_gain_intg.C       <= resize(r_error_curr.C * C_KI_INTG_GAIN, r_gain_intg.C);
+                r_gain_intg.d       <= resize(r_error_curr.d * C_KI_INTG_GAIN, r_gain_intg.d);
+                r_gain_intg.q       <= resize(r_error_curr.q * C_KI_INTG_GAIN, r_gain_intg.q);
             end if;
         end if;
     end process gains;
 
-    
+    accumulator : process(i_clk)
+        variable v_intg_accum : t_park_volt_record;
+    begin 
+        if rising_edge(i_clk) then 
+            if i_rst = '1' then 
+                r_intg_accum        <= (others => (others => '0'));
+            elsif r_state = ST_ACCUM then 
+                v_intg_accum.d      := r_gain_intg.d + r_intg_accum.d;
+                v_intg_accum.q      := r_gain_intg.q + r_intg_accum.q;
 
+                -- Clamp direct axis integral accumulator 
+                if v_intg_accum.d > C_WINDUP_MAX then 
+                    r_intg_accum.d  <= C_WINDUP_MAX;
+                elsif v_intg_accum.d < -C_WINDUP_MAX then 
+                    r_intg_accum.d  <= resize(-C_WINDUP_MAX, r_intg_accum.d);
+                else
+                    r_intg_accum.d  <= v_intg_accum.d;
+                end if;
+
+                -- Clamp quadrature axis integral accumulator 
+                if v_intg_accum.q > C_WINDUP_MAX then 
+                    r_intg_accum.q  <= C_WINDUP_MAX;
+                elsif v_intg_accum.q < -C_WINDUP_MAX then 
+                    r_intg_accum.q  <= resize(-C_WINDUP_MAX, r_intg_accum.q);
+                else
+                    r_intg_accum.q  <= v_intg_accum.q;
+                end if;
+            end if;
+        end if;
+    end process accumulator;
+
+    output : process(i_clk)
+        variable v_output_volt : t_park_volt_record;
+    begin 
+        if rising_edge(i_clk) then 
+            if i_rst = '1' then 
+                o_ph_voltage        <= (others => (others => '0'));
+            elsif r_state = ST_OUTPUT then 
+                v_output_volt.d     := r_intg_accum.d + r_gain_prop.d;
+                v_output_volt.q     := r_intg_accum.q + r_gain_prop.q;
+
+                -- Clamp direct axis voltage output
+                if v_output_volt.d > C_WINDUP_MAX then 
+                    o_ph_voltage.d  <= C_WINDUP_MAX;
+                elsif v_output_volt.d < -C_WINDUP_MAX then 
+                    o_ph_voltage.d  <= resize(-C_WINDUP_MAX, o_ph_voltage.d);
+                else
+                    o_ph_voltage.d  <= v_output_volt.d;
+                end if;
+
+                -- Clamp quadrature axis voltage output
+                if v_output_volt.q > C_WINDUP_MAX then 
+                    o_ph_voltage.q  <= C_WINDUP_MAX;
+                elsif v_output_volt.q < -C_WINDUP_MAX then 
+                    o_ph_voltage.q  <= resize(-C_WINDUP_MAX, o_ph_voltage.q);
+                else
+                    o_ph_voltage.q  <= v_output_volt.q;
+                end if;
+
+                o_vld <= '1';
+            else 
+                o_vld <= '0';
+            end if;
+        end if;
+    end process output;
 end architecture rtl;
